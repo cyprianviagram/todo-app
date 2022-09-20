e7ff0cf66b7d9621395246c9b1deb897831b8b2e
Mateusz Kozub <mckozub@gmail.com>
2022-09-20 16:33:49 +0200

    2

--- src/main/kotlin/io/github/cyprianviagram/todoapp/model/Task.kt
+++ src/main/kotlin/io/github/cyprianviagram/todoapp/model/Task.kt
@@ -1,32 +1,17 @@
 package io.github.cyprianviagram.todoapp.model
 
 import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
 import javax.persistence.Id
 import javax.persistence.Table
 
 @Entity
 @Table(name = "tasks")
-class Task {
-    @get:Id
-    private var id: Int
-        get() {
-            return id
-        }
-        set(id: Int) {
-            this.id = id
-        }
-    private var description: String
-        get() {
-            return description
-        }
-        set(description: String) {
-            this.description = description
-        }
-    private var done: Boolean
-        get() {
-            return done
-        }
-        set(done: Boolean) {
-            this.done = done
-        }
-}
+class Task (
+    var description: String? = null,
+    var done: Boolean? = null,
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    var id: Int? = null
+)
