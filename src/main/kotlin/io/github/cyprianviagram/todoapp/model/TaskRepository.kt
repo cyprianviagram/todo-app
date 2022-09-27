c85107167446248986af11a7f059957d974af0c4
Mateusz Kozub <mckozub@gmail.com>
2022-09-27 23:43:04 +0200

    forbade task deletion added new method findByDone

--- src/main/kotlin/io/github/cyprianviagram/todoapp/model/TaskRepository.kt
+++ src/main/kotlin/io/github/cyprianviagram/todoapp/model/TaskRepository.kt
@@ -1,8 +1,19 @@
 package io.github.cyprianviagram.todoapp.model
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.query.Param
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
+import org.springframework.data.rest.core.annotation.RestResource
+import java.util.*
 
 @RepositoryRestResource
 interface TaskRepository : JpaRepository<Task, Int>{
+    @RestResource(exported = false)
+    override fun deleteById(id: Int) {
+    }
+    @RestResource(exported = false)
+    override fun delete(entity: Task) {
+    }
+@RestResource(path = "done", rel = "done")
+    fun findByDone(@Param("state") done: Boolean): List<Task>
 }
