0d294ad3ea9bbe067b7be4311f2ad614e342c387
Mateusz Kozub <mckozub@gmail.com>
2022-09-28 15:40:14 +0200

    added controller

---
+++ src/main/kotlin/io/github/cyprianviagram/todoapp/controller/TaskController.kt
@@ -0,0 +1,21 @@
+package io.github.cyprianviagram.todoapp.controller
+
+import io.github.cyprianviagram.todoapp.model.TaskRepository
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.data.rest.webmvc.RepositoryRestController
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+
+@RepositoryRestController
+class TaskController (
+    private val repository: TaskRepository
+) {
+    private val logger: Logger = LoggerFactory.getLogger(javaClass)
+
+    @GetMapping("/tasks", params = ["!sort", "!page", "!size"])
+    fun readAllTasks(): ResponseEntity<Any?>{
+        logger.warn("Exposing all the tasks!")
+        return ResponseEntity.ok(repository.findAll())
+    }
+}
