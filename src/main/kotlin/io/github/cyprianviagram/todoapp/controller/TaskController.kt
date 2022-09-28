c2f98f96c001501b89bdb8c1473ec888a69877b3
Mateusz Kozub <mckozub@gmail.com>
2022-09-28 15:52:16 +0200

    added pageable method

--- src/main/kotlin/io/github/cyprianviagram/todoapp/controller/TaskController.kt
+++ src/main/kotlin/io/github/cyprianviagram/todoapp/controller/TaskController.kt
@@ -3,6 +3,7 @@ package io.github.cyprianviagram.todoapp.controller
 import io.github.cyprianviagram.todoapp.model.TaskRepository
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.data.domain.Pageable
 import org.springframework.data.rest.webmvc.RepositoryRestController
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -17,5 +18,11 @@ class TaskController (
     fun readAllTasks(): ResponseEntity<Any?>{
         logger.warn("Exposing all the tasks!")
         return ResponseEntity.ok(repository.findAll())
+    }
+
+    @GetMapping("/tasks")
+    fun readAllTasks(page: Pageable): ResponseEntity<Any?>{
+        logger.info("Custom pageable")
+        return ResponseEntity.ok(repository.findAll(page))
     }
 }
