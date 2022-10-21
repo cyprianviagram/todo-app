826115632e6acbf71d592c457fa114b53c95e1ff
Mateusz Kozub <mckozub@gmail.com>
2022-10-21 19:11:06 +0200

    properties hierarchy

--- src/main/kotlin/io/github/cyprianviagram/todoapp/TaskConfigurationProperties.kt
+++ src/main/kotlin/io/github/cyprianviagram/todoapp/TaskConfigurationProperties.kt
@@ -1,8 +1,16 @@
 package io.github.cyprianviagram.todoapp
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.stereotype.Component
+import java.awt.GraphicsConfigTemplate
+import kotlin.properties.Delegates
 
+@Component
 @ConfigurationProperties("task")
-data class TaskConfigurationProperties(
-    var allowMultipleTasksFromTemplate: Boolean? = null
-)
+class TaskConfigurationProperties {
+    var template = Template()
+
+    class Template {
+        var allowMultipleTasksFromTemplate by Delegates.notNull<Boolean>()
+    }
+}
--- src/main/kotlin/io/github/cyprianviagram/todoapp/controller/InfoController.kt
+++ src/main/kotlin/io/github/cyprianviagram/todoapp/controller/InfoController.kt
@@ -2,24 +2,23 @@ package io.github.cyprianviagram.todoapp.controller
 
 import io.github.cyprianviagram.todoapp.TaskConfigurationProperties
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties
-import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@EnableConfigurationProperties(TaskConfigurationProperties::class)
+// @EnableConfigurationProperties(TaskConfigurationProperties::class)
 class InfoController (
     val dataSource: DataSourceProperties,
     val myProp: TaskConfigurationProperties
 ) {
 
     @GetMapping("/info/url")
-    fun url(): String? {
+    fun url(): String {
         return dataSource.url
     }
 
     @GetMapping("/info/prop")
-    fun myProp(): Boolean? {
-        return myProp.allowMultipleTasksFromTemplate
+    fun myProp(): Boolean {
+        return myProp.template.allowMultipleTasksFromTemplate
     }
 }
